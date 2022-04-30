e2b1ef7965a18e5e21b98ebc4e14b725a06bbf8d
Leone67 <Leone67>
2022-04-30 05:55:30 +0000

    Added support for sequencing incoming rows

--- StreamJoin.py
+++ StreamJoin.py
@@ -1,6 +1,6 @@
 # Databricks notebook source
 from pyspark.sql import functions as F
-from pyspark.sql.functions import pandas_udf, PandasUDFType, struct, lit, col,count
+from pyspark.sql.window import Window
 import uuid
 from  pyspark import StorageLevel
 from functools import reduce
@@ -140,6 +140,7 @@ class StreamingJoin:
            selectCols,
            finalSelectCols):
     packed = self._left.stream().select(struct('*').alias('left'), lit(None).alias('right')).unionByName(self._right.stream().select(lit(None).alias('left'), struct('*').alias('right')))
+    spark.sparkContext.setLocalProperty("spark.scheduler.pool", str(uuid.uuid4()))
     return (
       packed
         .writeStream 
@@ -167,7 +168,18 @@ class StreamToStreamJoinWithConditionForEachBatch:
     self._joinKeys = joinKeys
     self._selectCols = selectCols
     self._finalSelectCols = finalSelectCols
-    
+  
+  def _safeMergeLists(self, l, r):
+    a = l
+    if r is not None:
+      if a is None:
+        a = r
+      else:
+        a = a + r
+    if a is not None:
+      a = list(dict.fromkeys(a))
+    return a
+
   def foreachBatch(self, mergeFunc):
     return StreamingJoin(self._left,
                self._right,
@@ -176,39 +188,36 @@ class StreamToStreamJoinWithConditionForEachBatch:
                                self._selectCols,
                                self._finalSelectCols)
 
-  def _doMerge(self, deltaTable, cond, batchDf, batchId):
+  def _doMerge(self, deltaTable, cond, primaryKeys, windowSpec, matchCondition, batchDf, batchId):
+    if windowSpec is not None:
+      batchDf = batchDf.withColumn('row_number', F.row_number().over(windowSpec)).where('row_number = 1')
+    else:
+      batchDf = batchDf.dropDuplicates(primaryKeys)
     deltaTable.alias("u").merge(
         source = batchDf.alias("staged_updates"),
         condition = F.expr(cond)) \
-        .whenMatchedUpdateAll() \
+        .whenMatchedUpdateAll(condition = matchCondition) \
         .whenNotMatchedInsertAll() \
         .execute()
 
-  def join(self, right, storagePath = None):
-    if storagePath is None:
-      dir = os.path.dirname(right.path())
-      name = f'{os.path.basename(self._left.path())}_{os.path.basename(self._right.path())}_{os.path.basename(right.path())}'
-      m = hashlib.sha256()
-      m.update(self._left.path().encode('ascii'))
-      m.update(self._right.path().encode('ascii'))
-      m.update(right.path().encode('ascii'))
-      storagePath = f'{dir}/{name}/{m.hexdigest()}'
-    (
-      self.writeToPath(f'{storagePath}/data')
-          .option('checkpointLocation', f'{storagePath}/cp')
-          .start()
-    )
-    return Stream.fromPath(f'{storagePath}/data').primaryKeys(*list(dict.fromkeys(self._left.getPrimaryKeys() + self._right.getPrimaryKeys()))).join(right)
-
-  def writeToPath(self, path):
+  def _writeToTarget(self, deltaTableForFunc, tableName, path):
     ddl = self._left.static().join(self._right.static(),
                                                    self._joinExpr(self._left.static(), self._right.static())).select(self._finalSelectCols(self._left.static(),
                           self._right.static())).schema.toDDL()
-    spark.sql(f'DROP TABLE IF EXISTS delta.`{path}`')
-    spark.sql(f'CREATE TABLE delta.`{path}`({ddl}) USING DELTA')
-    cond = ' AND '.join([f'u.{k} = staged_updates.{k}' for k in list(dict.fromkeys(self._left.getPrimaryKeys() + self._right.getPrimaryKeys()))])
+    createSql = f'CREATE TABLE IF NOT EXISTS {tableName}({ddl}) USING DELTA'
+    if path is not None:
+      createSql = f"{createSql} LOCATION '{path}'"
+    spark.sql(createSql)
+    primaryKeys = self._safeMergeLists(self._left.getPrimaryKeys(), self._right.getPrimaryKeys())
+    sequenceColumns = self._safeMergeLists(self._left.getSequenceColumns(), self._right.getSequenceColumns())
+    cond = ' AND '.join([f'u.{k} = staged_updates.{k}' for k in primaryKeys])
+    windowSpec = None
+    matchCondition = None
+    if primaryKeys is not None and len(primaryKeys) > 0 and sequenceColumns is not None and len(sequenceColumns) > 0:
+      windowSpec = Window.partitionBy(primaryKeys).orderBy([F.desc(sc) for sc in sequenceColumns])
+      matchCondition = ' AND '.join([f'u.{sc} <= staged_updates.{sc}' for sc in sequenceColumns])
     def mergeFunc(batchDf, batchId):
-      self._doMerge(DeltaTable.forPath(spark, path), cond, batchDf, batchId)
+      self._doMerge(deltaTableForFunc(), cond, primaryKeys, windowSpec, matchCondition, batchDf, batchId)
 
     return StreamingJoin(self._left,
                self._right,
@@ -216,19 +225,32 @@ class StreamToStreamJoinWithConditionForEachBatch:
                                self._joinKeys,
                                self._selectCols,
                                self._finalSelectCols)
+
+  def generateJoinStagingPath(self, right):
+    dir = os.path.dirname(right.path())
+    name = f'$$_{os.path.basename(self._left.path())}_{os.path.basename(self._right.path())}_{os.path.basename(right.path())}'
+    m = hashlib.sha256()
+    m.update(self._left.path().encode('ascii'))
+    m.update(self._right.path().encode('ascii'))
+    m.update(right.path().encode('ascii'))
+    return f'{dir}/{name}/{m.hexdigest()}'
+
+  def join(self, right, stagingPath = None):
+    if stagingPath is None:
+      stagingPath = self.generateJoinStagingPath(right)
+    (
+      self.writeToPath(f'{stagingPath}/data')
+          .option('checkpointLocation', f'{stagingPath}/cp')
+          .start()
+    )
+    return Stream.fromPath(f'{stagingPath}/data').primaryKeys(*self._safeMergeLists(self._left.getPrimaryKeys(), self._right.getPrimaryKeys())).join(right)
+    
+  def writeToPath(self, path):
+    return self._writeToTarget(lambda: DeltaTable.forPath(spark, path), f'delta.`{path}`', path)
 
   def writeToTable(self, tableName):
-    cond = ' AND '.join([f'u.{k} = staged_updates.{k}' for k in list(dict.fromkeys(self._left.getPrimaryKeys() + self._right.getPrimaryKeys()))])
-    def mergeFunc(batchDf, batchId):
-      self._doMerge(DeltaTable.forName(spark, tableName), cond, batchDf, batchId)
-
-    return StreamingJoin(self._left,
-               self._right,
-               mergeFunc).join(self._joinExpr,
-                               self._joinKeys,
-                               self._selectCols,
-                               self._finalSelectCols)
-
+    return self._writeToTarget(lambda: DeltaTable.forName(spark, tableName), tableName, None)
+      
 class ColumnSelector:
   _stream = None
   _columnName = None
@@ -360,6 +382,7 @@ class Stream:
   _stream = None
   _static = None
   _primaryKeys = None
+  _sequenceColumns = None
   _path = None
 
   def __init__(self,
@@ -369,7 +392,7 @@ class Stream:
     self._static = static
   
   @staticmethod
-  def fromPath(path):
+  def fromPath(path, startingVersion = None):
     stream = Stream(spark.readStream.format('delta').option("readChangeFeed", "true").load(path).where("_change_type != 'update_preimage'").drop('_change_type', '_commit_version', '_commit_timestamp'),
                  spark.read.format('delta').load(path))
     stream.setPath(path)
@@ -401,6 +424,13 @@ class Stream:
   
   def getPrimaryKeys(self):
     return self._primaryKeys
+
+  def sequenceBy(self, *columns):
+    self._sequenceColumns = columns
+    return self
+  
+  def getSequenceColumns(self):
+    return self._sequenceColumns
 
   def join(self, right):
     return StreamToStreamJoin(self, right)
