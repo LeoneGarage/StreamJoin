b0725bc5b54ca0f940706f6d7f1dfb2a6fa0c32b
Leone67 <Leone67>
2023-05-20 08:58:59 +0000

    Added parameter to point to a different repo subdir for StreamJoin modules

--- StreamJoin.py
+++ StreamJoin.py
@@ -1,13 +1,20 @@
 # Databricks notebook source
+dbutils.widgets.text("repo_subdir", "StreamJoin", "StreamJoin repo subdirectory")
+
+# COMMAND ----------
+
+repo_subdir = dbutils.widgets.get("repo_subdir")
+
+# COMMAND ----------
+
 import sys
 from pathlib import Path
 user = dbutils.notebook.entry_point.getDbutils().notebook().getContext().tags().apply('user')
-path = Path(dbutils.notebook.entry_point.getDbutils().notebook().getContext().extraContext().apply('notebook_path')).parent
 try:
-  sys.path.remove(f'/Workspace/Repos/{user}/{path.name}')
+  sys.path.remove(f'/Workspace/Repos/{user}/{repo_subdir}')
 except ValueError as e:
   pass
-sys.path.insert(1, f'/Workspace/Repos/{user}/{path.name}')
+sys.path.insert(1, f'/Workspace/Repos/{user}/{repo_subdir}')
 
 # COMMAND ----------
 
